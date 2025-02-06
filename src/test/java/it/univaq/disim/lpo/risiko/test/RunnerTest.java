5c009a8d06539977970ecdf035fd3935f0e95b5e
master <161362468+AlexMarinucci99@users.noreply.github.com>
2025-02-06 14:32:00 +0100

    Risiko

---
+++ src/test/java/it/univaq/disim/lpo/risiko/test/RunnerTest.java
@@ -0,0 +1,23 @@
+package it.univaq.disim.lpo.risiko.test;
+
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.univaq.disim.lpo.risiko.core.Runner;
+
+class RunnerTest {
+	
+	public Runner it;
+
+	@BeforeEach
+	public void setUp() {
+		it = new Runner();
+	}
+
+	@Test
+	void test() {
+	}
+
+}
